8c45c86bf2e95dc6cecbcb268c1b6e1ad99dcbad
John Olmsted <null>
2014-01-14 16:10:44 -0800

    Adds solution to release 0

--- source/serialization.rb
+++ source/serialization.rb
@@ -1,29 +1,36 @@
 require 'csv'
 
 class Person
-  # Look at the above CSV file
-  # What attributes should a Person object have?
+  def initialize(table)
+    @id = table['id']
+    @first_name = table['first_name']
+    @last_name = table['last_name']
+    @email = table['email']
+    @phone = table['phone']
+    @created_at = table['created_at']
+  end
 end
 
 class PersonParser
   attr_reader :file
-  
+
   def initialize(file)
     @file = file
     @people = nil
   end
-  
+
   def people
-    # If we've already parsed the CSV file, don't parse it again.
-    # Remember: @people is +nil+ by default.
     return @people if @people
-    
-    # We've never called people before, now parse the CSV file
-    # and return an Array of Person objects here.  Save the
-    # Array in the @people instance variable.
+    @people = Array.new
+    CSV.foreach(@file, :headers => true) do |table|
+      @people << Person.new(table)
+    end
   end
 end
 
-parser = PersonParser.new('people.csv')
 
+#-----DRIVERS-----
+
+parser = PersonParser.new('people.csv')
+parser.people
 puts "There are #{parser.people.size} people in the file '#{parser.file}'."
