74fd4f5332840051b4cd41161d4ed5958b25c378
David Goodman & Ricardo A. Rubio <pair+dgoodman1224.rickarubio@devbootcamp.com>
2014-01-14 16:06:40 -0800

    release 0, power to the parsed people

--- source/serialization.rb
+++ source/serialization.rb
@@ -3,27 +3,40 @@ require 'csv'
 class Person
   # Look at the above CSV file
   # What attributes should a Person object have?
+  def initialize(id, first_name, last_name, email, phone, created_at)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @phone = phone
+    @created_at = created_at
+  end
 end
 
 class PersonParser
   attr_reader :file
-  
+
   def initialize(file)
     @file = file
-    @people = nil
+    @people = []
   end
-  
+
   def people
+   CSV.foreach(@file) do |row|
+      @people << Person.new(row[0], row[1], row[2], row[3], row[4], row[5])
+    end
+  @people.shift
     # If we've already parsed the CSV file, don't parse it again.
     # Remember: @people is +nil+ by default.
-    return @people if @people
-    
     # We've never called people before, now parse the CSV file
     # and return an Array of Person objects here.  Save the
     # Array in the @people instance variable.
+    return @people if @people
   end
 end
 
+
 parser = PersonParser.new('people.csv')
+
 
 puts "There are #{parser.people.size} people in the file '#{parser.file}'."
