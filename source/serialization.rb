d3ed46cc4defe90b075d801a3eac1f27907c5827
Emmanuel Kaunitz & Matthew Higgins <pair+mechamanny.mh120888@devbootcamp.com>
2014-01-14 16:49:46 -0800

    long lists of data, serialization works, parsing a file.

--- source/serialization.rb
+++ source/serialization.rb
@@ -1,23 +1,34 @@
 require 'csv'
 
 class Person
-  # Look at the above CSV file
-  # What attributes should a Person object have?
+  attr_reader :id, :first_name, :last_name, :email, :phone, :created_at
+  def initialize(id, first_name, last_name, email, phone, created_at)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @phone = phone
+    @created_at = created_at
+  end
 end
 
 class PersonParser
   attr_reader :file
-  
+
   def initialize(file)
     @file = file
-    @people = nil
+    @people = []
   end
-  
+
   def people
+    source_file = CSV.read(@file)
+    source_file.each do |row|
+      @people << Person.new(row[0], row[1], row[2], row[3], row[4], row[5])
+    end
     # If we've already parsed the CSV file, don't parse it again.
     # Remember: @people is +nil+ by default.
     return @people if @people
-    
+
     # We've never called people before, now parse the CSV file
     # and return an Array of Person objects here.  Save the
     # Array in the @people instance variable.
@@ -25,5 +36,5 @@ class PersonParser
 end
 
 parser = PersonParser.new('people.csv')
-
+parser.people
 puts "There are #{parser.people.size} people in the file '#{parser.file}'."
