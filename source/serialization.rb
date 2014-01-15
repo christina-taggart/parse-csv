9c2ec619381056abd4640fd7cff865ec6cb576ad
David Goodman & Ricardo A. Rubio <pair+dgoodman1224.rickarubio@devbootcamp.com>
2014-01-14 17:20:39 -0800

    release 1: added ability to write to csv file

--- source/serialization.rb
+++ source/serialization.rb
@@ -1,8 +1,7 @@
 require 'csv'
 
 class Person
-  # Look at the above CSV file
-  # What attributes should a Person object have?
+  attr_reader :id, :first_name, :last_name, :email, :phone, :created_at
   def initialize(id, first_name, last_name, email, phone, created_at)
     @id = id
     @first_name = first_name
@@ -22,21 +21,35 @@ class PersonParser
   end
 
   def people
-   CSV.foreach(@file) do |row|
-      @people << Person.new(row[0], row[1], row[2], row[3], row[4], row[5])
+  # @people = [] # reset people
+   CSV.foreach(@file, :headers => true) do |row|
+      @people << Person.new(row['id'], row['first_name'], row['last_name'], row['email'], row['phone'], row['created_at'])
     end
-  @people.shift
-    # If we've already parsed the CSV file, don't parse it again.
-    # Remember: @people is +nil+ by default.
-    # We've never called people before, now parse the CSV file
-    # and return an Array of Person objects here.  Save the
-    # Array in the @people instance variable.
     return @people if @people
+  end
+
+  def display_people
+    @people
+  end
+
+  def add_person(instance_of_person)
+    @people << instance_of_person
+  end
+
+  def save
+    CSV.open(@file, "a") do |csv|
+      csv << [@people[-1].id, @people[-1].first_name, @people[-1].last_name, @people[-1].email, @people[-1].phone, @people[-1].created_at]
+    end
   end
 end
 
 
 parser = PersonParser.new('people.csv')
-
-
-puts "There are #{parser.people.size} people in the file '#{parser.file}'."
+david = Person.new(201,"David","Goodman","rickismybestfriend@gmail.com","516726736","76239873268235867")
+parser.add_person(david)
+parser.save
+rick = Person.new(202,"Ricardo","Rubio","davidisbestbuddyforever@aol.com","123726736","37629873268235867")
+parser.add_person(rick)
+puts "----------------------"
+parser.save
+puts "There are #{parser.people.size-2} people in the file '#{parser.file}'."#<Person:0x007fbe6283d790 @id=nil, @first_name=nil, @last_name=nil, @email=nil, @phone=nil, @created_at=nil>, #<Person:0x007fbe6283c2c8 @id=nil, @first_name=nil, @last_name=nil, @
