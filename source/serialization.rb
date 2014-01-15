9351814c49a6f1b86ad368cf2a03e6a5aa0b65ff
Emily Foley & Aditya Mahesh <pair+EmFoley.amahesh0501@devbootcamp.com>
2014-01-14 16:42:42 -0800

    Release 0, 1 and 2. CSV rows are like the best of both array and hash  worlds. And they are accessed like hashes.

--- source/serialization.rb
+++ source/serialization.rb
@@ -1,29 +1,48 @@
 require 'csv'
+require 'date'
 
 class Person
-  # Look at the above CSV file
-  # What attributes should a Person object have?
+  attr_reader :id, :first_name, :last_name, :email, :phone, :created_at
+  def initialize(id, first_name, last_name, email, phone, created_at)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @phone = phone
+    @created_at = created_at
+  end
 end
 
 class PersonParser
   attr_reader :file
-  
+
   def initialize(file)
     @file = file
     @people = nil
   end
-  
+
   def people
-    # If we've already parsed the CSV file, don't parse it again.
-    # Remember: @people is +nil+ by default.
     return @people if @people
-    
-    # We've never called people before, now parse the CSV file
-    # and return an Array of Person objects here.  Save the
-    # Array in the @people instance variable.
+    @people = []
+    CSV.foreach("people.csv", :headers => true) do |row|
+      @people << row
+      #p row["created_at"]
+      row["created_at"] = DateTime.parse(row["created_at"])
+    end
+    @people
   end
+
+
+  def add_people(person_added) #need to instantiate a new Person object and pass it in
+    array_to_add = [person_added.id, person_added.first_name, person_added.last_name, person_added.email, person_added.phone, person_added.created_at]
+    @people << array_to_add
+  end
+
+
 end
 
 parser = PersonParser.new('people.csv')
+p parser.people
+p parser.add_people(Person.new(201, "alex", "smith", "asmith@gmail.com", "1233433434", "2012-05-13T21:05:15-07:00"))
 
 puts "There are #{parser.people.size} people in the file '#{parser.file}'."
